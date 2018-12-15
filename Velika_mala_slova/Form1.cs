c8c27d13a8893bd4ac1fd751fdb490429de5dc9d
Bruno Akrap <brakrap@vsite.hr>
2018-12-15 12:13:18 +0100

    Napravljena funkcija SvaVelika().

--- Velika_mala_slova/Form1.cs
+++ Velika_mala_slova/Form1.cs
@@ -16,5 +16,32 @@ namespace Velika_mala_slova
         {
             InitializeComponent();
         }
+
+        public string SvaVelika()
+        {
+            return richTextBox1.SelectedText.ToUpper();
+        }
+
+        /*
+        public string PremaPravopisu()
+        {
+            richTextBox1.SelectedText.ToLowerInvariant();
+            if (richTextBox1.SelectedText.Contains("."))
+            {
+                string[] podjela = richTextBox1.SelectedText.Split('.');
+                for (int i = 0; i < podjela.Length; ++i)
+                {
+                    richTextBox1.SelectedText.Substring(i, podjela[i].Length).ToLowerInvariant();
+                }
+                
+            }
+            return "";
+        }
+        */
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
     }
 }
