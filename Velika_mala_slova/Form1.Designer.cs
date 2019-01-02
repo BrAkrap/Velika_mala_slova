51d50f83e906a540ac07528bf21237054359f08d
Bruno Akrap <brakrap@vsite.hr>
2019-01-02 18:03:25 +0100

    Isprogramiran gump "Import".

--- Velika_mala_slova/Form1.Designer.cs
+++ Velika_mala_slova/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.svaVelikaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.premaPravopisuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -41,6 +45,7 @@
             this.richTextBox1.Size = new System.Drawing.Size(265, 217);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // button1
             // 
@@ -50,6 +55,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "<-- Import...";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -59,15 +65,41 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Export... -->";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(402, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.svaVelikaToolStripMenuItem,
+            this.premaPravopisuToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // svaVelikaToolStripMenuItem
+            // 
+            this.svaVelikaToolStripMenuItem.Name = "svaVelikaToolStripMenuItem";
+            this.svaVelikaToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.svaVelikaToolStripMenuItem.Text = "Sva velika";
+            this.svaVelikaToolStripMenuItem.Click += new System.EventHandler(this.svaVelikaToolStripMenuItem_Click);
+            // 
+            // premaPravopisuToolStripMenuItem
+            // 
+            this.premaPravopisuToolStripMenuItem.Name = "premaPravopisuToolStripMenuItem";
+            this.premaPravopisuToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.premaPravopisuToolStripMenuItem.Text = "Prema pravopisu";
+            this.premaPravopisuToolStripMenuItem.Click += new System.EventHandler(this.premaPravopisuToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -80,6 +112,8 @@
             this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,6 +125,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem svaVelikaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem premaPravopisuToolStripMenuItem;
     }
 }
 
--- Velika_mala_slova/Form1.cs
+++ Velika_mala_slova/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,30 +17,107 @@ namespace Velika_mala_slova
         {
             InitializeComponent();
         }
-
-        public string SvaVelika()
+        
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
-            return richTextBox1.SelectedText.ToUpper();
+
         }
 
-        /*
-        public string PremaPravopisu()
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-            richTextBox1.SelectedText.ToLowerInvariant();
+            
+        }
+
+        private void svaVelikaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectedText = richTextBox1.SelectedText.ToUpper();
+        }
+
+        private void premaPravopisuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectedText = richTextBox1.SelectedText.ToLower();
+            
+            // provjera za točku
             if (richTextBox1.SelectedText.Contains("."))
             {
                 string[] podjela = richTextBox1.SelectedText.Split('.');
+                int pom = 0;
                 for (int i = 0; i < podjela.Length; ++i)
                 {
-                    richTextBox1.SelectedText.Substring(i, podjela[i].Length).ToLowerInvariant();
-                }
-                
+                    // provjera da li riječ počinje sa "nj", "dž" ili "lj"
+                    if (podjela[i].Substring(0,2).Equals("dž") || podjela[i].Substring(0, 2).Equals("nj") 
+                        || podjela[i].Substring(0, 2).Equals("lj"))
+                    {
+                        
+                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 2).ToUpper());
+                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 2).ToUpper();
+                    }
+                    else
+                    {
+                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 1).ToUpper());
+                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 1).ToUpper();
+                    }
+                    pom += podjela[i].Length;
+                }   
             }
-            return "";
-        }
-        */
+            // provjera za uskličnik
+            if (richTextBox1.SelectedText.Contains("!"))
+            {
+                string[] podjela = richTextBox1.SelectedText.Split('!');
+                int pom = 0;
+                for (int i = 0; i < podjela.Length; ++i)
+                {
+                    // provjera da li riječ počinje sa "nj", "dž" ili "lj"
+                    if (podjela[i].Substring(0, 2).Equals("dž") || podjela[i].Substring(0, 2).Equals("nj")
+                        || podjela[i].Substring(0, 2).Equals("lj"))
+                    {
 
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 2).ToUpper());
+                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 2).ToUpper();
+                    }
+                    else
+                    {
+                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 1).ToUpper());
+                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 1).ToUpper();
+                    }
+                    pom += podjela[i].Length;
+                }
+            }
+            // provjera za upitnik
+            if (richTextBox1.SelectedText.Contains("?"))
+            {
+                string[] podjela = richTextBox1.SelectedText.Split('?');
+                int pom = 0;
+                for (int i = 0; i < podjela.Length; ++i)
+                {
+                    // provjera da li riječ počinje sa "nj", "dž" ili "lj"
+                    if (podjela[i].Substring(0, 2).Equals("dž") || podjela[i].Substring(0, 2).Equals("nj")
+                        || podjela[i].Substring(0, 2).Equals("lj"))
+                    {
+
+                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 2).ToUpper());
+                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 2).ToUpper();
+                    }
+                    else
+                    {
+                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 1).ToUpper());
+                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 1).ToUpper();
+                    }
+                    pom += podjela[i].Length;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // IMPORT
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(open.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // EXPORT
         {
 
         }
