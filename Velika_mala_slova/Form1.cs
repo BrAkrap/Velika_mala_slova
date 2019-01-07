0dd60b559650bf7450ccbd7cd72c5801586a95ab
Bruno Akrap <brakrap@vsite.hr>
2019-01-07 20:33:39 +0100

    Isprogramirana funkcija "Prema pravopisu".

--- Velika_mala_slova/Form1.cs
+++ Velika_mala_slova/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace Velika_mala_slova
 {
@@ -35,79 +36,48 @@ namespace Velika_mala_slova
 
         private void premaPravopisuToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            richTextBox1.SelectedText = richTextBox1.SelectedText.ToLower();
+            int duljina = richTextBox1.SelectionLength;
+            int pocetak = richTextBox1.SelectionStart;
+            bool pocetakRecenice = false;
+            string pom = richTextBox1.SelectedText;
+            StringBuilder rezultantniTekst = new StringBuilder();
             
-            // provjera za točku
-            if (richTextBox1.SelectedText.Contains("."))
-            {
-                string[] podjela = richTextBox1.SelectedText.Split('.');
-                int pom = 0;
-                for (int i = 0; i < podjela.Length; ++i)
-                {
-                    // provjera da li riječ počinje sa "nj", "dž" ili "lj"
-                    if (podjela[i].Substring(0,2).Equals("dž") || podjela[i].Substring(0, 2).Equals("nj") 
-                        || podjela[i].Substring(0, 2).Equals("lj"))
-                    {
-                        
-                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 2).ToUpper());
-                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 2).ToUpper();
-                    }
-                    else
-                    {
-                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 1).ToUpper());
-                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 1).ToUpper();
-                    }
-                    pom += podjela[i].Length;
-                }   
-            }
-            // provjera za uskličnik
-            if (richTextBox1.SelectedText.Contains("!"))
-            {
-                string[] podjela = richTextBox1.SelectedText.Split('!');
-                int pom = 0;
-                for (int i = 0; i < podjela.Length; ++i)
-                {
-                    // provjera da li riječ počinje sa "nj", "dž" ili "lj"
-                    if (podjela[i].Substring(0, 2).Equals("dž") || podjela[i].Substring(0, 2).Equals("nj")
-                        || podjela[i].Substring(0, 2).Equals("lj"))
-                    {
+            richTextBox1.SelectedText = richTextBox1.SelectedText.ToLower();
 
-                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 2).ToUpper());
-                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 2).ToUpper();
-                    }
-                    else
+            if (pocetak == 0)
+            {
+                pocetakRecenice = true;
+            }
+
+            for (int i = 0; i < duljina; i++)
+            {
+                char slovo = pom[i];
+                
+                if (Char.IsLetterOrDigit(slovo) && pocetakRecenice == true)
+                {
+                    rezultantniTekst.Append(Char.ToUpper(slovo));
+                    pocetakRecenice = false;
+                }
+                else if (slovo == ' ')
+                {
+                    rezultantniTekst.Append(" ");
+                }
+                else
+                {
+                    rezultantniTekst.Append(slovo);
+                    if (slovo == '.' || slovo == '!' || slovo == '?')
                     {
-                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 1).ToUpper());
-                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 1).ToUpper();
+                        pocetakRecenice = true;
                     }
-                    pom += podjela[i].Length;
                 }
             }
-            // provjera za upitnik
-            if (richTextBox1.SelectedText.Contains("?"))
-            {
-                string[] podjela = richTextBox1.SelectedText.Split('?');
-                int pom = 0;
-                for (int i = 0; i < podjela.Length; ++i)
-                {
-                    // provjera da li riječ počinje sa "nj", "dž" ili "lj"
-                    if (podjela[i].Substring(0, 2).Equals("dž") || podjela[i].Substring(0, 2).Equals("nj")
-                        || podjela[i].Substring(0, 2).Equals("lj"))
-                    {
 
-                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 2).ToUpper());
-                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 2).ToUpper();
-                    }
-                    else
-                    {
-                        richTextBox1.SelectedText.Substring(0, 2).Replace(richTextBox1.SelectedText.Substring(0, 2), richTextBox1.SelectedText.Substring(pom, 1).ToUpper());
-                        //richTextBox1.SelectedText.Substring(i * podjela[i - 1].Length, 1).ToUpper();
-                    }
-                    pom += podjela[i].Length;
-                }
-            }
+            richTextBox1.Select(pocetak, duljina);
+            richTextBox1.SelectedText = rezultantniTekst.ToString();
+            
         }
 
+        
         private void button1_Click(object sender, EventArgs e) // IMPORT
         {
             OpenFileDialog open = new OpenFileDialog();
